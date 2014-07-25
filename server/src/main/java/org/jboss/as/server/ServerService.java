a89dc0ea29d5001dca9be96e67408431e76fd555
Brian Stansberry <brian.stansberry@redhat.com>
2014-07-24 21:17:40 -0500

    ExtensionRegistry should not cache any ManagementResourceRegistration so it won't use the wrong one if it's cloned

--- server/src/main/java/org/jboss/as/server/ServerService.java
+++ server/src/main/java/org/jboss/as/server/ServerService.java
@@ -246,7 +246,8 @@ public final class ServerService extends AbstractControllerService {
                             }
                         },
                         authorizer,
-                        super.getAuditLogger()));
+                        super.getAuditLogger(),
+                        getMutableRootResourceRegistrationProvider()));
         super.start(context);
     }
 
